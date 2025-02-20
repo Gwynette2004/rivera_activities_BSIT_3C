8263017b16f5e1860e5da1b7247fcd948fd88bf9
Gwynette2004 <157551832+Gwynette2004@users.noreply.github.com>
2025-02-20 11:52:40 +0800

    Activity 2

--- RiveraPortfolio/base/views.py
+++ RiveraPortfolio/base/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'base/home.html',)
+    data = [
+        {"title": "Projects", "count": 4},
+        {"title": "Skills", "count": 5},
+        {"title": "Dean's Listers", "count": 8},
+    ]
+    # Pass the data to the template
+    return render(request, 'base/home.html', {'data': data})
