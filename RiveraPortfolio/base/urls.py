b16076ce13c79d560fae75cb90d0a7b52ccc2ebc
Gwynette2004 <157551832+Gwynette2004@users.noreply.github.com>
2025-03-07 22:24:31 +0800

    added rest framework

--- RiveraPortfolio/base/urls.py
+++ RiveraPortfolio/base/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from .views import ItemListCreateView, ItemDetailView, ItemDeleteView, ItemUpdateView
 
 
 urlpatterns = [
-    path('',views.home)
+    path('',views.home, name="home"),
+    path('items/', ItemListCreateView.as_view(),name='item-list-create'),
+    path('items/<int:pk>/', ItemDetailView.as_view(), name='item-detail'),
+    path('items/update/<int:pk>/', ItemUpdateView.as_view(), name='item-update'),
+    path('items/delete/<int:pk>/', ItemDeleteView.as_view(), name='item-delete'),
 ]
--- RiveraPortfolio/base/views.py
+++ RiveraPortfolio/base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Item
+from .serializer import ItemSerializer
 # Create your views here.
 
 def home(request):
@@ -10,3 +12,24 @@ def home(request):
     ]
     # Pass the data to the template
     return render(request, 'base/home.html', {'data': data})
+
+
+# get all items and create a new item in one
+class ItemListCreateView(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+#get one item
+class ItemDetailView(generics.RetrieveAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+#update an item
+class ItemUpdateView(generics.UpdateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+#delete an item
+class ItemDeleteView(generics.DestroyAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
